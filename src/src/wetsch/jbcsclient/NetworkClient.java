8d9f2396d91b13b4e910b83f3c5a03fa76943168
corruptcube <gamerkcw123@hotmail.com>
2016-07-03 13:23:47 -0700

    push changes

--- src/src/wetsch/jbcsclient/NetworkClient.java
+++ src/src/wetsch/jbcsclient/NetworkClient.java
@@ -8,42 +8,71 @@ import java.net.Socket;
 import java.util.HashSet;
 import java.util.Set;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
 /*
- * Last modified: 6/8/2016
- * Changes:
- * Added command support.
- */
+* Last modified 7/1/2016
+ * Added Registration commands.
+ * Added client message types to indicate how to display the message to the user.
+  */
 
 /**
  * This Async Task connects to the JBCS-Server and sends the data. 
  * @author kevin
  *
  */
-public class NetworkClient extends AsyncTask<String[], Integer, Object>{
-	String tag = this.getClass().getName();//holds the activities full path.class name.
+public class NetworkClient extends AsyncTask<String[], Integer, Object[]>{
+	
+	private Context context = null;//The activity that invoked this async task.
+	private String tag = this.getClass().getName();//holds the activities full path.class name.
+	private String connectionFaildError = null;////Holds the connection error message
 	private Socket connection = null;//Connection object.
 	private BufferedReader in = null;//Connections input stream.
 	private PrintWriter out = null;//Connections output stream.
 	private String host = null;//Listening server's IP address.
 	private int port = 0;//Listening server's port number. 
 	private Set<JBCSClientListener> listener;
-
+	private ProgressDialog pd = null;//Dialdog used to show client wait status.
+	//Dialog indecators
+	private final int pdWaitForServer = 0;//Tell the dialog that it is waiting for the server.
+	private final int pdDone = 1;//Tell the dialog it is done and can close.
+	/**
+	 * Client Error dialog message.
+	 * Used if the client encountered an error, such as a connection error.
+	 */
+	public static final int clientErrorDialogMessage = 2;
+	/**
+	 * Client Notification dialog message.
+	 * Used when the server responded back with a message.
+	 */
+	public static final int clientInfoDialogMessage = 3;
+	/**
+	 * Client notification Toast Message.
+	 * Used when the server received the barcode data.
+	 */
+	public static final int clientInfoToastMessage = 4;
+	
 	/**
 	 * Takes the listening server's IP address, port number and a context
 	 * @param host Listening server's IP address.
 	 * @param port Listening server's port number.
 	 * @param context The context of the activity that triggered the task.
 	 */
-	public NetworkClient(String host, int port, JBCSClientListener listener) {
+	public NetworkClient(String host, int port, JBCSClientListener listener, Context context) {
+		this.context = context;
 		this.host = host;
 		this.port =  port;
 		if(listener != null){
 			this.listener = new HashSet<JBCSClientListener>();
 			this.listener.add(listener);
 		}
+		connectionFaildError = context.getString(R.string.connection_error_message);
+		pd = new ProgressDialog(context);
+		pd.setTitle("Client Status");
+		pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
 	}
 	
 	public void addjbcsClientListener(JBCSClientListener listener){
@@ -71,9 +100,9 @@ public class NetworkClient extends AsyncTask<String[], Integer, Object>{
 	
 	//This method runs on a background thread.  It's job is to send the data to the server.
 	@Override
-	protected Object doInBackground(String[]... params) {
+	protected Object[] doInBackground(String[]... params) {
 		String[] data = params[0];
-		Object serverResponce = null;
+		Object serverResponce[] = null;
 		
 		try {
 			connection = new Socket(host, port);
@@ -81,21 +110,56 @@ public class NetworkClient extends AsyncTask<String[], Integer, Object>{
 			out = new PrintWriter(connection.getOutputStream());
 			
 			switch (data[0]) {
-				case "SEND_BARCODE_DATA":
+				case ServerCommands.sendBarcodeData:
 					out.println(data[0]);
-					out.println(data[1]);
-					out.println(data[2]);
 					out.flush();
-					serverResponce = in.readLine();
-				break;
-			case "CHECK_CONNECTION":
+					String drsStatus = in.readLine();
+					if(drsStatus.equals(ServerCommands.drsEnabled)){
+						out.println(data[1]);
+						out.flush();
+						String registrationStatus = in.readLine();
+						if(registrationStatus.equals(ServerCommands.unregistered))
+							serverResponce = new Object[]{clientInfoDialogMessage, context.getString(R.string.unregistered_device_message)};
+						else if(registrationStatus.equals(ServerCommands.registered)){
+							out.println(data[2]);
+							out.println(data[3]);
+							out.flush();
+							if(in.readLine().equals(ServerCommands.dataReceived))
+								serverResponce = new Object[]{clientInfoToastMessage, context.getString(R.string.data_received_message)};
+						}
+					}else if(drsStatus.equals(ServerCommands.drsDisabled)){
+						out.println(data[2]);
+						out.println(data[3]);
+						out.flush();
+						if(in.readLine().equals(ServerCommands.dataReceived))
+							serverResponce = new Object[]{clientInfoToastMessage, context.getString(R.string.data_received_message)};
+					}
+					break;
+			case ServerCommands.checkConnection:
 				out.println(data[0]);
 				out.flush();
-				serverResponce = in.readLine();
+				if(in.readLine().equals(ServerCommands.connectionOk))
+					serverResponce = new Object[]{clientInfoDialogMessage, context.getString(R.string.connection_ok_message)};
+				break;
+			case ServerCommands.registerDevice:
+				out.println(data[0]);
+				out.println(data[1]);
+				out.flush();
+				publishProgress(pdWaitForServer);
+				String message = in.readLine();
+				if(message.equals(ServerCommands.registered))
+					serverResponce = new Object[]{clientInfoDialogMessage, context.getString(R.string.device_already_registered_message)};
+				else if(message.equals(ServerCommands.registrationRequestInactive))
+					serverResponce = new Object[]{clientInfoDialogMessage, context.getString(R.string.registration_request_inactive_message)};
+				else if(message.equals(ServerCommands.deviceRegistered))
+					serverResponce = new Object[]{clientInfoDialogMessage, context.getString(R.string.Device_registered_message)};
+				else if(message.equals(ServerCommands.deviceRejected))
+					serverResponce = new Object[]{clientInfoDialogMessage, context.getString(R.string.device_rejected_message)};
+				publishProgress(pdDone);
 				break;
 
 			default:
-				serverResponce = "Invalid command";
+				serverResponce = new Object[]{clientErrorDialogMessage, "Client received an invalid command."};
 				break;
 			}
 			closeConnection();
@@ -105,16 +169,80 @@ public class NetworkClient extends AsyncTask<String[], Integer, Object>{
 			Log.e(tag, "Client failed to transmit data to the server.");
 			e.printStackTrace();
 		}
-		return null;
+		return new Object[]{clientErrorDialogMessage, connectionFaildError};
 	}
 	
+	
+	
+	@Override
+	protected void onProgressUpdate(Integer... values) {
+		if(values[0] == pdWaitForServer){
+			pd.setMessage("Waiting for server.");
+			pd.show();
+		}else if(values[0] == pdDone)
+			pd.dismiss();
+		super.onProgressUpdate(values);
+	}
+
 	//This method runs after the doInBackground method.  It sends messages back to the user.
 	@Override
-	protected void onPostExecute(Object result) {
+	protected void onPostExecute(Object[] result) {
 		if(listener != null){
 			for(JBCSClientListener l : listener)
 				l.onJbcsClientFinish(result);
 		}
 		super.onPostExecute(result);
+	}
+	
+	public static class ServerCommands{
+		/**
+		 * Command to check the server connection.
+		 */
+		public static final String checkConnection = "CHECK_CONNECTION";
+		/**
+		 * Command to register a new device.
+		 */
+		public static final String registerDevice = "REGISTER_DEVICE";
+		/**
+		 * Server response to indicate the connection is OK.
+		 */
+		public static final String connectionOk = "CONNECTION_OK";
+		/**
+		 * Command to send the barcode data.
+		 */
+		public static final String sendBarcodeData = "SEND_BARCODE_DATA";
+		/**
+		 * Server response to indicate the barcode data was received.
+		 */
+		public static final String dataReceived = "BC_DATA_RECEIVED";
+		/**
+		 * Server response to indicate the device is not registered with the server.
+		 */
+		public static final String unregistered = "UNREGISTERED";
+		/**
+		 * Server response to indicate the device is registered with the server.
+		 */
+		public static final String registered = "REGISTERED";
+		/**
+		 * Server response to indicate the device registration request was accepted by the server.
+		 */
+		public static final String deviceRegistered = "DEVICE_REGISTERED";
+		/**
+		 * Server response to indicate the device registration request was rejected by the server.
+		 */
+		public static final String deviceRejected = "DEVICE_REJECTED";
+		/**
+		 * Server response to indicate the Device registration system in enforced.
+		 */
+		public static final String drsEnabled = "DRS_ENABLED"; 
+		/**
+		 * Server response to indicate the device registration system is not enforced.
+		 */
+		public static final String drsDisabled = "DRS_DISABLED";
+		/**
+		 * Server response to indicate the server is not accepting registration requests.
+		 */
+		public static final String registrationRequestInactive = "REGISTRATION_REQUEST_INACTIVE";
+		
 	}
 }
--- src/src/wetsch/jbcsclient/NetworkClientPreferences.java
+++ src/src/wetsch/jbcsclient/NetworkClientPreferences.java
@@ -1,46 +1,82 @@
 package wetsch.jbcsclient;
 
 import wetsch.jbcsclient.R;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 /*
- * Last modified: 6/8/2016
- * Changes:
- * converted Connection request to a comand passed to the client.
- */
+* Last modified 7/1/2016
+ * Added Registration commands.
+ * Added client message types to indicate how to display the message to the user.
+  */
 
+@SuppressLint("DrawAllocation")
 public class NetworkClientPreferences extends Activity implements OnClickListener, JBCSClientListener{
-	private String filename;
+	private SharedPreferences pref;//The object to access the shared preferences that hold the settings for the client.
 	
-	private SharedPreferences pref;
+	private TextView TextViewInstallID;
+	private EditText edtHostIPAddress;//The server's host name or IP address
+	private EditText edtPortNumber;//The server port number. 
+	private Button btnSavePref;// Save the settings for the client.
+	private Button btnCheckConfig;//Check if configuration is correct to connect to server.
+	private Button btnRegisterDevice;//Register device with server.
 	
-	private EditText edtHostIPAddress;
-	private EditText edtPortNumber;
-	private Button btnSavePref;
-	private Button btnCheckConfig;
-	private ToggleButton tgbUseClient;;
+	private ToggleButton tgbUseClient;//Turn on/off client.
 	private AlertDialog clientMessageDialog;//Message dialog for the network client.
-
+	private String SID;//The ID to be used to register the device with the server.
 	
+	//The method called to setup the activity.
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.network_client_pref);
 		
+		setContentView(R.layout.network_client_pref);
+		SID = Installation.id(this);
 		setupObjects();
 		setupClientMessageDialog();
-		LoadPreferences();
+		SetDefaultPreferences();
+		//Setting cursor position on text fields.
+		setCursorSelection(edtHostIPAddress);
+		setCursorSelection(edtPortNumber);
 	}
+	
+	
+	//Inflates the option menu.
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.netclientmenu, menu);
+		return true;
+	}
+	
+	//Handles the menu items selected.
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.nc_menuitem_Send_test_Vaue:
+			sendTestBarcodeValueAction();
+			break;
+		}
+		return true;
+	}
+
 	
 	//Set up the message dialog for the client.
 	private void setupClientMessageDialog(){
@@ -53,23 +89,57 @@ public class NetworkClientPreferences extends Activity implements OnClickListene
 			        }
 			    });	
 	}
-
 	
+	//apply Background
+	private void setBackground(){
+		DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+		int h = metrics.heightPixels;
+        GradientDrawable gd = new GradientDrawable();
+        gd.setColors(new int[]{Color.argb(255, 0, 0, 255), Color.argb(255, 0, 0, 0)});
+        gd.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        gd.setGradientRadius(h/2);
+        gd.setGradientCenter(0.5f,0.5f);
+        LinearLayout l = (LinearLayout) findViewById(R.id.network_client_pref);
+        l.setBackground(gd);
+	}       
+
+		
+		
+
+	//Set up the UI objects
 	private void setupObjects(){
-		filename = "network_client_preferences";
-		pref = getSharedPreferences(filename, MODE_PRIVATE);;
+		pref = getSharedPreferences(Tools.networkClientPreferencesPath, MODE_PRIVATE);;
 		edtHostIPAddress = (EditText) findViewById(R.id.edt_net_client_host_IP);
 		edtPortNumber = (EditText) findViewById(R.id.edt_net_client_port);
 		btnSavePref = (Button) findViewById(R.id.btn_save_pref);
 		btnSavePref.setOnClickListener(this);
 		btnCheckConfig = (Button) findViewById(R.id.btn_check_config);
 		btnCheckConfig.setOnClickListener(this);
+		btnRegisterDevice = (Button) findViewById(R.id.btn_register_device);
+		btnRegisterDevice.setOnClickListener(this);
 		tgbUseClient = (ToggleButton) findViewById(R.id.tgl_net_client_use);
+		TextViewInstallID = (TextView) findViewById(R.id.tv_install_id);
+		TextViewInstallID.setText("SID:" +SID);
+		setBackground();
 		
 	}
-
 	
-	private void LoadPreferences(){
+	/*
+	 * By default, if there is text in a text-field, the cursor is positioned to the left.
+	 * this method sets it to the right if there is text in the field. 
+	 */
+	private void setCursorSelection(EditText object){
+		if(object.getText().length() != 0){
+			object.setSelection(object.getText().length());
+		}
+	}
+
+	/*
+	 * Checks if the preferences have been set by the user.
+	 * If the Preferences have not been set yes, defaults are
+	 * used.
+	 */
+	private void SetDefaultPreferences(){
 		if(!pref.contains("host_name")){
 			Editor e = pref.edit();
 			e.putString("host_name", "");
@@ -90,10 +160,12 @@ public class NetworkClientPreferences extends Activity implements OnClickListene
 		edtHostIPAddress.setText(pref.getString("host_name", ""));
 		edtPortNumber.setText(pref.getString("port_number", "9800"));
 		tgbUseClient.setChecked(pref.getBoolean("use_client", false));
-		
-
 	}
 	
+	/*
+	 * Saves the client Preferences set by the user.
+	 * The save button calls this method.
+	 */
 	private void savePreferences(){
 		Editor e = pref.edit();
 		e.putString("host_name", edtHostIPAddress.getText().toString());
@@ -102,15 +174,60 @@ public class NetworkClientPreferences extends Activity implements OnClickListene
 		e.commit();
 	}
 	
+	/*
+	 * Send barcode test data to the server.
+	 */
+	private void sendTestBarcodeValueAction(){
+		String host = edtHostIPAddress.getText().toString();
+		int port = Integer.parseInt(edtPortNumber.getText().toString());
+		String serverCommand = NetworkClient.ServerCommands.sendBarcodeData;
+		new NetworkClient(host, port, this, this).execute(new String[]{serverCommand, SID, "CODE_39", "0123456789"});
+	}
+	
+	/*
+	 * This method checks the client settings to make sure they are correct.
+	 * If they are correct, the server will respond back indicating that the
+	 * connection is okey, otherwise an error will be displayed.
+	 */
 	private void checkConnection(){
 		String host = edtHostIPAddress.getText().toString();
 		int port = Integer.parseInt(edtPortNumber.getText().toString());
-		new NetworkClient(host, port, this).execute(new String[]{"CHECK_CONNECTION"});
-
+		String serverCommand = NetworkClient.ServerCommands.checkConnection;
+		new NetworkClient(host, port, this, this).execute(new String[]{serverCommand});
+	}
+	
+	/*
+	 * This method sends the registration request to register the device with the server.
+	 * A confirmation dialog will pop-up asking the user to press ready when the user is
+	 * ready to send the request to the server.
+	 */
+	private void RegisterDevice(){
+		AlertDialog dialog = new AlertDialog.Builder(this).create();
+		dialog.setTitle(getString(R.string.register_device_dialog_title));
+		dialog.setMessage(getString(R.string.register_device_dialog_message));
+		dialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.btn_ready), new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				String host = edtHostIPAddress.getText().toString();
+				int port = Integer.parseInt(edtPortNumber.getText().toString());
+				String serverCommand = NetworkClient.ServerCommands.registerDevice;
+				new NetworkClient(host, port, NetworkClientPreferences.this, NetworkClientPreferences.this).execute(new String[]{serverCommand, SID});
+			}
+		});
+		dialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.btn_Cancel), new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		dialog.show();
 	}
 	
 	//Listeners
 	
+	//Listener for the buttons
 	@Override
 	public void onClick(View v) {
 		if(v.getId() == R.id.btn_save_pref){
@@ -118,18 +235,27 @@ public class NetworkClientPreferences extends Activity implements OnClickListene
 			finish();
 		}else if(v.getId() == R.id.btn_check_config){
 			checkConnection();
+		}else if(v.getId() == R.id.btn_register_device){
+			RegisterDevice();
 		}
 	}
 	
+	//Listener for the client dialog messages.
 	@Override
 	public void onJbcsClientFinish(Object clientMessage) {
-		if(clientMessage == null){
-			clientMessageDialog.setMessage(getString(R.string.connection_error_message));
+		Object[] message =  (Object[]) clientMessage;
+		int messageType = (int) message[0];
+		String messageString = (String) message[1];
+		if(messageType == NetworkClient.clientErrorDialogMessage){
+			clientMessageDialog.setTitle("Client Error");
+			clientMessageDialog.setMessage(messageString);
 			clientMessageDialog.show();
-		}else{
-			clientMessageDialog.setMessage((String)clientMessage);
+		}else if(messageType == NetworkClient.clientInfoDialogMessage){
+			clientMessageDialog.setTitle("Client Message");
+			clientMessageDialog.setMessage(messageString);
 			clientMessageDialog.show();
-
-		}
+		}else if(messageType == NetworkClient.clientInfoToastMessage)
+			Toast.makeText(this, messageString, Toast.LENGTH_SHORT).show();
 	}
+	
 }
